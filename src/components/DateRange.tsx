08ec7102c04062dc6305deb8ab2449a2a65bdfcd
koke <rekoke92@gmail.com>
2023-06-12 18:56:11 +0200

    date range styled

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -1,6 +1,7 @@
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css";
 import "react-calendar/dist/Calendar.css";
+import "../styles/MyDateRangeStyles.css";
 import styles from "../styles/dateRange.module.css";
 
 export const DateRange = ({ todayEnd, dates, setDates }: any) => {
