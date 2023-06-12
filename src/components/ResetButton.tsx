e5ca24f73a0ed0d3768f74ccbd8c59c5a9aa82e7
koke <rekoke92@gmail.com>
2023-06-12 18:56:43 +0200

    clean button styles

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/resetButton.module.css";
 const ResetButton = ({ unclickCountries }: any) => {
   return (
     <div className={styles.resetButton}>
-      <button onClick={unclickCountries}>Clean</button>
+      <button onClick={unclickCountries}>Clean map</button>
     </div>
   );
 };
