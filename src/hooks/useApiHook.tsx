d0ff7f5dcf9823685a330e88d58ca7355586f0e3
koke <rekoke92@gmail.com>
2023-10-03 14:14:18 +0200

    API hook updated and app better performance

--- src/hooks/useApiHook.tsx
+++ src/hooks/useApiHook.tsx
@@ -6,14 +6,24 @@ export type TApiResponse = {
   data: any;
   error: any;
   loading: Boolean;
+  execute: () => void;
 };
 
-export const useApiGet = (url: string, deps: any = null): TApiResponse => {
+export const useApiGet = (
+  url: string,
+  dates: any = null,
+  fromSearch: boolean = false
+): TApiResponse => {
   const [status, setStatus] = useState<Number>(0);
   const [statusText, setStatusText] = useState<String>("");
   const [data, setData] = useState<any>();
   const [error, setError] = useState<any>();
   const [loading, setLoading] = useState<boolean>(false);
+  const [toCallApi, setApiExecution] = useState(false);
+
+  const execute = () => {
+    setApiExecution(true);
+  };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const getAPIData = async () => {
@@ -29,11 +39,18 @@ export const useApiGet = (url: string, deps: any = null): TApiResponse => {
     }
     setLoading(false);
   };
-
   useEffect(() => {
-    getAPIData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [deps]);
+    if (fromSearch) {
+      if (toCallApi) {
+        getAPIData();
+        setApiExecution(false);
+      }
+    } else {
+      getAPIData();
+    }
 
-  return { status, statusText, data, error, loading };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toCallApi, dates, fromSearch]);
+
+  return { status, statusText, data, error, loading, execute };
 };
